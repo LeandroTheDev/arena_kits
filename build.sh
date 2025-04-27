5317187c235181686bb55fb92fc60309f6379fb7
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2025-04-27 11:46:50 -0300

    Add missing library

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-game_dir=".../SteamLibrary/steamapps/common/U3DS/Servers/TestArena/Rocket/Plugins/"
-library_dir=".../SteamLibrary/steamapps/common/U3DS/Servers/TestArena/Rocket/Libraries/"
+game_dir="/home/bobs/System/Devices/Hard Drive/SteamLibrary/steamapps/common/U3DS/Servers/playtoearn3/Rocket/Plugins/"
+library_dir="/home/bobs/System/Devices/Hard Drive/SteamLibrary/steamapps/common/U3DS/Servers/playtoearn3/Rocket/Libraries/"
 
 dotnet build -c Release
 cp -r ./bin/Release/net4.8/ArenaKits.dll "$game_dir"
+cp -r ./Libs/MySql.Data.dll "$library_dir"
+cp -r ./Libs/Ubiety.Dns.Core.dll "$library_dir"
